1c6443194b4bd996d6a328b2e4f331943a83bc78
Patryk Brzoza <kyrtap@users.noreply.github.com>
2024-09-28 12:35:49 +0200

    Make windows draggable

--- src/scripts.js
+++ src/scripts.js
@@ -13,6 +13,8 @@ function spawnWindow() {
     newWindow.style.top = newTop + 'px';
 
     newWindow.querySelector('.button').onclick = spawnWindow;
+
+    makeWindowDraggable(newWindow);
 }
 
 function centerWindow() {
@@ -33,8 +35,41 @@ function updateClock() {
     document.getElementById('clock').textContent = hours + ':' + minutes;
 }
 
+function makeWindowDraggable(windowElement) {
+    var titleBar = windowElement.querySelector('.title-bar');
+    var offsetX = 0, offsetY = 0, initialX = 0, initialY = 0, isDragging = false;
+
+    titleBar.onmousedown = function(e) {
+        isDragging = true;
+        initialX = e.clientX;
+        initialY = e.clientY;
+        var rect = windowElement.getBoundingClientRect();
+        offsetX = initialX - rect.left;
+        offsetY = initialY - rect.top;
+
+        document.onmousemove = function(e) {
+            if (isDragging) {
+                var newX = e.clientX - offsetX;
+                var newY = e.clientY - offsetY;
+                windowElement.style.left = newX + 'px';
+                windowElement.style.top = newY + 'px';
+            }
+        };
+
+        document.onmouseup = function() {
+            isDragging = false;
+            document.onmousemove = null;
+            document.onmouseup = null;
+        };
+    };
+}
+
 window.onload = function() {
     centerWindow();
     updateClock();
     setInterval(updateClock, 1000);
+
+    // Make the initial window draggable
+    var messageWindow = document.getElementById('messageWindow');
+    makeWindowDraggable(messageWindow);
 };
